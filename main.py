149e9f58ca73fe2f0d3546e9111159512ad81703
Charles <charles.de-dampierre@hec.edu>
2022-05-20 13:51:57 +0200

    fix bugs

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pandas as pd
 from bunka_topics.basic_class import BasicSemantics
 
-df = pd.read_csv("data/imdb.csv", index_col=[0])
-df = df.sample(1000, random_state=42)
-df = df[["imdb", "description"]].dropna()
 
-# instantiate model
-model = BasicSemantics(df, index_var="imdb", text_var="description")
+if __name__ == "__main__":
 
-model.fit(
-    extract_terms=True,
-    terms_embeddings=True,
-    docs_embeddings=True,
-    sample_size_terms=10000,
-    terms_limit=3000,
-    terms_ents=True,
-    language="en",
-)
+    df = pd.read_csv("data/imdb.csv", index_col=[0])
+    df = df.sample(1000, random_state=42)
+    df = df[["imdb", "description"]].dropna()
+
+    # instantiate model
+    model = BasicSemantics(df, index_var="imdb", text_var="description")
+
+    model.fit(
+        extract_terms=True,
+        terms_embeddings=True,
+        docs_embeddings=True,
+        sample_size_terms=10000,
+        terms_limit=3000,
+        terms_ents=True,
+        language="en",
+    )
+
+    terms = model.terms

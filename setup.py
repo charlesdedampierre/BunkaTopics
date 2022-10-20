f663898ad5f1a9e0a36e8dee0c9fabeff3a73e09
Charles De Dampierre <charles.de-dampierre@hec.edu>
2022-10-20 12:36:59 +0200

    feat{clustering}: ass the possibility to change the number of reduced dimensions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="bunkatopics",
     packages=["bunkatopics"],
-    version="0.20",
+    version="0.21",
     license="MIT",
     description="Advanced Topic Modeling Using Transformers",
     author="Charles de Dampierre",

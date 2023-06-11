7f0d62d08b18a02e9eaa778766975dc5bfe4f0f2
Charles De Dampierre <charles.de-dampierre@hec.edu>
2023-06-11 10:10:47 +0200

    do a unique test pipeline

--- tests/test_bunka.py
+++ tests/test_bunka.py
@@ -5,21 +5,22 @@ sys.path.append("../")
 import unittest
 from bunkatopics import Bunka
 import pandas as pd
+import plotly.graph_objects as go
+from sklearn.datasets import fetch_20newsgroups
+import random
 
 
 class BunkaTestCase(unittest.TestCase):
     def setUp(self):
+        docs = fetch_20newsgroups(
+            subset="all", remove=("headers", "footers", "quotes")
+        )["data"]
+        docs = random.sample(docs, 100)
         self.bunka = Bunka()
-        docs = [
-            "john is in the kitchen",
-            "Therese is coding",
-            "Charles is playing outside",
-            "Someon is coming to the house",
-        ]
-
         self.bunka.fit(docs)
 
-    def test_fit_transform(self):
+    def pipeline(self):
+        # Run the full pipelune
         n_clusters = 2
         df_topics = self.bunka.get_topics(n_clusters=n_clusters)
 
@@ -27,15 +28,24 @@ class BunkaTestCase(unittest.TestCase):
         self.assertEqual(len(df_topics), n_clusters)
         self.assertIsInstance(df_topics, pd.DataFrame)
 
-    def test_search(self):
+        bourdieu_fig = self.bunka.visualize_bourdieu(
+            x_left_words=["past"],
+            x_right_words=["future", "futuristis"],
+            y_top_words=["politics", "Government"],
+            y_bottom_words=["cultural phenomenons"],
+            height=2000,
+            width=2000,
+            clustering=False,
+        )
+
+        self.assertIsInstance(bourdieu_fig, go.Figure)
+
         user_input = "this is a computer"
         results = self.bunka.search(user_input)
 
         # Add assertions to verify the expected output
         self.assertIsNotNone(results)
         self.assertIsInstance(results, pd.DataFrame)
-
-    # Add more test methods for other functions in the Bunka class
 
 
 if __name__ == "__main__":

961c1f1f60f1e3dece55c5170e38052c8dcfe727
Charles De Dampierre <charles.de-dampierre@hec.edu>
2023-10-17 22:02:19 +0200

    change api

--- api/datamodel.py
+++ api/datamodel.py
@@ -7,3 +7,8 @@ from bunkatopics.datamodel import Document, Topic
 class BourdieuResponse(BaseModel):
     bourdieu_docs: t.List[Document]
     bourdieu_topics: t.List[Topic]
+
+
+class BunkaResponse(BaseModel):
+    docs: t.List[Document]
+    topics: t.List[Topic]

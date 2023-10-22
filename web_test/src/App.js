843ec30ec54456e0e69ccbd7f244a988ed9977f0
Charles De Dampierre <charles.de-dampierre@hec.edu>
2023-10-22 17:16:03 +0200

    make chaneg front

--- web_test/src/App.js
+++ web_test/src/App.js
@@ -1,6 +1,21 @@
-import JsonDisplay from './JsonDisplay'
+import React from 'react';
+import Map from './Map';
+import CsvUpload from './CsvUpload'; // Import the CsvUpload component
 
 const App = () => {
-  return (<JsonDisplay />)
+  return (
+    <div className="App">
+      <div className="json-display">
+        <div className="top-right">
+          <a href="https://www.linkedin.com/company/company-profile-link" target="_blank" rel="noopener noreferrer" className="linkedin-icon">
+            <img src="/linkedin_logo.png" alt="LinkedIn" />
+          </a>
+          <img src="/bunka_logo.png" alt="Bunka Logo" className="bunka-logo" />
+        </div>
+        <Map />
+      </div>
+    </div>
+  );
 }
+
 export default App;

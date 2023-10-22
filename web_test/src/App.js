5033e66432128560b86460a3449960d053efdd28
Charles De Dampierre <charles.de-dampierre@hec.edu>
2023-10-22 17:40:53 +0200

    front

--- web_test/src/App.js
+++ web_test/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import Map from './Map';
-import CsvUpload from './CsvUpload'; // Import the CsvUpload component
+import React, { useState } from 'react';
+import DropdownMenu from './DropdownMenu';
+import Map from './Map'; // Rename from JsonDisplay to Map
+import DocsView from './DocsView';
 
 const App = () => {
+  const [selectedView, setSelectedView] = useState('map'); // Default to 'map'
+
   return (
     <div className="App">
       <div className="json-display">
         <div className="top-right">
-          <a href="https://www.linkedin.com/company/company-profile-link" target="_blank" rel="noopener noreferrer" className="linkedin-icon">
+          <a
+            href="https://www.linkedin.com/company/company-profile-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkedin-icon"
+          >
             <img src="/linkedin_logo.png" alt="LinkedIn" />
           </a>
           <img src="/bunka_logo.png" alt="Bunka Logo" className="bunka-logo" />
+          <DropdownMenu onSelectView={setSelectedView} />
         </div>
-        <Map />
+        {selectedView === 'map' ? <Map /> : <DocsView />}
       </div>
     </div>
   );
-}
+};
 
 export default App;

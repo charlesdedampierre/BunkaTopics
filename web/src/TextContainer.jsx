98974a35fab6dcb1ef79a37ee52c89374682ccbd
elias@showk.me <elias@showk.me>
2023-12-20 17:21:07 +0100

    fix document text icon

--- web/src/TextContainer.jsx
+++ web/src/TextContainer.jsx
@@ -7,7 +7,7 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import DescriptionIcon from "@mui/icons-material/Description"; // Import the document icon
+import AbcIcon from '@mui/icons-material/Abc';
 
 function TextContainer({ topicName, sizeFraction, content }) {
   const [selectedDocument, setSelectedDocument] = useState(null);
@@ -56,7 +56,7 @@ function TextContainer({ topicName, sizeFraction, content }) {
             {content.map((doc, index) => (
               <ListItem button key={doc.id} onClick={() => handleDocumentClick(index)} selected={selectedDocument === index}>
                 <ListItemIcon>
-                  <DescriptionIcon /> {/* Display a document icon */}
+                  <AbcIcon variant="filled"/> {/* Display a document icon */}
                 </ListItemIcon>
                 <ListItemText primary={<span style={{ fontSize: "16px" }}>{doc}</span>} />
               </ListItem>

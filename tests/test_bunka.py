9a6c6d3b941056f6621869cc68b61f5f94dd704d
Charles De Dampierre <charles.de-dampierre@hec.edu>
2024-01-16 19:45:23 +0100

    Finish BunkaClass documentation

--- tests/test_bunka.py
+++ tests/test_bunka.py
@@ -30,13 +30,13 @@ class TestBunka(unittest.TestCase):
         # Load a sample dataset
         dataset = load_dataset("rguo123/trump_tweets")
         docs = dataset["train"]["content"]
-        docs = random.sample(docs, 200)
+        docs = random.sample(docs, 2000)
         cls.bunka = Bunka()
         cls.bunka.fit(docs)
 
     def test_topic_modeling(self):
         # Test Topic Modeling
-        n_clusters = 3
+        n_clusters = 10
         df_topics = self.bunka.get_topics(n_clusters=n_clusters, min_count_terms=1)
         print(df_topics.name)
         self.assertIsInstance(df_topics, pd.DataFrame)

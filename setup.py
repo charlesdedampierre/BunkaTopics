70234347cb9628236e48a6710c50cfc7b3abed0b
Charles De Dampierre <charles.de-dampierre@hec.edu>
2024-01-21 20:53:39 +0100

    add ipython

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dependencies = [
     "psutil>=5.9.7",
     "colorlog>=6.8.0",
     "langchain_openai",
-    "IPython",
+    "ipython",
 ]
 
 test = ["nbformat>=4.2.0", "ipywidgets", "nbconvert", "jupyter"]
